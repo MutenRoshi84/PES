ea6d5f7b1a3bdac714ed78b54166add71c97beb0
Timon Ulrich <timon.ulrich@kabelmail.de>
2017-11-13 16:02:25 +0100

    kleine Änderung an commitReveal.sol

--- commitReveal.sol
+++ commitReveal.sol
@@ -7,14 +7,20 @@ contract commitReveal {
     
     //Commitment
     function commit (bytes32 _in) internal {
+        
         commits[msg.sender] = _in;
+        
     }
     
     //Reveal
     function reveal (string _secret, uint8 _seed, uint8 _guess) constant internal returns (bool correct) {
+        
+        correct = false;
+        
         if (commits[msg.sender] == keccak256 (_secret, _seed, _guess)) {
             correct = true;
         }
+        
     }
     
 }
